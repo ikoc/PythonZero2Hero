bfbdf1f16ea83b6836b8e30507afa561dd0ccba2
Ibrahim Koc <ikoc@ekin.com>
2023-04-13 18:33:04 +0300

    Recursion iteration comment added .

--- projects/project4.py
+++ projects/project4.py
@@ -1,5 +1,5 @@
 # Fibonacci sequence: Write a program that generates the Fibonacci sequence up to a given number using loops
-# and conditional statements.
+# and conditional statements. Use recursion and iteration approach. 
 def main():
     ## Write your code below
     return
